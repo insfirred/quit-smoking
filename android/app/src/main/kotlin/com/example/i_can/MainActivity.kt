4b4cadbf5a6723e04f6d46633aef0f8d2d96bd22
insfirred <kalash27k@gmail.com>
2022-10-05 18:11:16 +0530

    Project initiated...

---
+++ android/app/src/main/kotlin/com/example/i_can/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.i_can
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1F2120)
+    );
+  }
+}
