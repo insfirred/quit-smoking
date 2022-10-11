b12c1955d67f1788727a4ea97bed408351d55ba7
insfirred <kalash27k@gmail.com>
2022-10-11 14:45:59 +0530

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:i_can/controllers/user_controller.dart';
